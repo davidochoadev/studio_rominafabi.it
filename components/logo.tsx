2f5e385de184d65d4385cb37e7c4c9dafa828042
davidochoadev <davidochoa.dev@gmail.com>
2024-02-04 00:39:31 +0100

    🚀 First Commit

---
+++ components/logo.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+const Logo = () => {
+  return (
+    <img
+      style={{width: '25px', marginTop: '0', marginBottom: '0px'}}
+      src="../static/logo.png"
+      alt="Shwick logo"
+    />
+  )
+}
+
+export default Logo
--- sanity.config.ts
+++ sanity.config.ts
@@ -2,11 +2,12 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
+import Logo from './components/logo'
 
 export default defineConfig({
   name: 'default',
   title: 'rominafabi.it',
-
+  icon: Logo,
   projectId: 'huvthnv7',
   dataset: 'production',
 
